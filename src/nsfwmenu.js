3289e2b5b444a52f7f6ad4a51b706d669848794d
HyoukaGK <hyoukagk787@gmail.com>
2021-02-16 13:38:21 -0300

    First commit

---
+++ src/nsfwmenu.js
@@ -0,0 +1,31 @@
+const nsfwmenu = (prefix, pushname) => {
+    return `OPA SAFRADO KKKK, AQUI É O MENU DE *NSFW*
+ 
+ ❗PARA UTILIZAR OS COMANDOS ABAIXO DE O SEGUINTE COMANDO:\n ${prefix}nsfw 1\n *
+
+➩ ${prefix}nsfwbobs
+➩ ${prefix}randomhentaio
+➩ ${prefix}nsfwtrap
+➩ ${prefix}nsfwass
+➩ ${prefix}nsfwbelly
+➩ ${prefix}nsfwsidebobs
+➩ ${prefix}nsfwahegao
+➩ ${prefix}nsfwthighs
+➩ ${prefix}nsfwarmpits
+➩ ${prefix}nsfwfeets
+
+                        「ʀ ᴇ ɢ ᴜ ʀ ᴜ s」
+╔═════════════════════════════
+║   BOT TRADUZIDO POR: K1R1T0&DANIEL                         
+║   NUMERO DO DONO: wa.me/5544999862424 
+║   NUMERO DO DANIEL: wa.me/556593494107
+║   PARA MAIS INFORMAÇÕES DIGITE
+║ ➩${prefix}dono
+╚═════════════════════════════
+`
+
+
+
+}
+
+exports.nsfwmenu = nsfwmenu
